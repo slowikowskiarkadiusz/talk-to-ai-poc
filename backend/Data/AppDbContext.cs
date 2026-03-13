eaa142ce8fb35f15b44a14f131204b7e922226f5
slowikowskiarkadiusz <slowikowskiarkadiusz@gmail.com>
2026-03-13 08:13:58 +0100

    removed backend

--- backend/Data/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-using API.Models;
-
-namespace API.Data;
-
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-{
-    public DbSet<PlantEntry> PlantEntries => Set<PlantEntry>();
-}
